b7dd42f7ac657a58e643ccd08ba817187c922590
Alexander <alxndr+github@gmail.com>
2015-12-08 22:41:07 -0800

    day 8 part 2 in ruby

--- 08.rb
+++ 08.rb
@@ -1,1 +1,5 @@
-IO.readlines("./08.txt").map{|s| ss = s.strip; {chars: ss.size, size: (eval ss).length}  }.reduce({chars: 0, size: 0}) { |acc, elem| {chars: acc[:chars] + elem[:chars], size: acc[:size] + elem[:size] } }
+def newcount(str)
+  str.length + 2 + str.scan(/"/).count + str.scan(%r{\\}).count
+end
+
+IO.readlines("./08.txt").map{|s| ss = s.strip; {chars: ss.size, size: (eval ss).length, quoted: newcount(ss) }  }.reduce({chars: 0, size: 0, quoted: 0}) { |acc, elem| {chars: acc[:chars] + elem[:chars], size: acc[:size] + elem[:size], quoted: acc[:quoted] + elem[:quoted] } }
