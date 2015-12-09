07ad344d3c8af3a3050d3a02afd605faa91d6fd7
Alexander <alxndr+github@gmail.com>
2015-12-08 22:14:17 -0800

    day 7 part 1 in elixir done
    whew

--- 07.exs
+++ 07.exs
@@ -34,6 +34,7 @@ defmodule Santa do
     value =
       mapping
       |> Map.fetch!(var)
+      |> IO.inspect
       |> calculate_value(mapping)
     if value < 0 do
       value + 65536
@@ -42,42 +43,47 @@ defmodule Santa do
     end
   end
 
-  # n.b. this recursion is not optimized
-  def calculate_value([value], _values) when value in "0".."9999" do
-    IO.puts "to integer: #{value}"
-    String.to_integer(value)
+  def calculate_value(n, values) when is_integer(n), do: {n, values}
+  def calculate_value([value], values) when value in "0".."9999" do
+    new_value = String.to_integer(value)
+    {new_value, values}
   end
   def calculate_value([variable], values) when variable in "a".."zz" do
-    IO.puts "look up: #{variable}"
     {:ok, value} = Map.fetch(values, variable)
-    calculate_value(value, values)
+    {new_value, new_values} = calculate_value(value, values)
+    newer_values = Map.put(new_values, variable, new_value) # here's the real memoization?
+    {new_value, newer_values}
   end
   def calculate_value(["NOT" | instructions], values) do
     IO.puts "not: #{inspect instructions}"
-    value = calculate_value(instructions, values)
-    Bitwise.bnot(value)
+    {new_value, new_values} = calculate_value(instructions, values)
+    {Bitwise.bnot(new_value), new_values}
   end
-  def calculate_value([variable1, "AND", variable2], values) do
-    IO.puts "and: #{inspect variable1}, #{inspect variable2}"
-    Bitwise.band(calculate_value([variable1], values), calculate_value([variable2], values))
-  end
-  def calculate_value([variable1, "OR", variable2], values) do
-    IO.puts "or: #{inspect variable1}, #{inspect variable2}"
-    Bitwise.bor(calculate_value([variable1], values), calculate_value([variable2], values))
-  end
-  def calculate_value([variable1, "LSHIFT", variable2], values) do
-    IO.puts "lshift: #{inspect variable1}, #{inspect variable2}"
-    Bitwise.bsl(calculate_value([variable1], values), calculate_value([variable2], values))
-  end
-  def calculate_value([variable1, "RSHIFT", variable2], values) do
-    IO.puts "rshift: #{inspect variable1}, #{inspect variable2}"
-    Bitwise.bsr(calculate_value([variable1], values), calculate_value([variable2], values))
+  def calculate_value([variable1, operation, variable2], values) do
+    {new_value1, new_values} = calculate_value([variable1], values)
+    newer_values = Map.put(new_values, variable1, new_value1)
+    {new_value2, newerr_values} = calculate_value([variable2], newer_values)
+    newest_values = Map.put(newerr_values, variable2, new_value2)
+    {handle_operation(new_value1, operation, new_value2), newest_values}
   end
   def calculate_value(instructions, values) do
     IO.puts "uh oh... something broke"
     IO.inspect instructions
     IO.puts "==========="
     IO.inspect values
+  end
+
+  def handle_operation(leftval, "AND", rightval) do
+    Bitwise.band(leftval, rightval)
+  end
+  def handle_operation(leftval, "OR", rightval) do
+    Bitwise.bor(leftval, rightval)
+  end
+  def handle_operation(leftval, "LSHIFT", rightval) do
+    Bitwise.bsl(leftval, rightval)
+  end
+  def handle_operation(leftval, "RSHIFT", rightval) do
+    Bitwise.bsr(leftval, rightval)
   end
 
 end
