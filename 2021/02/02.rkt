430c5806995b3d869e0eba7724bc53f292c1f426
Alexander <alxndr+github@gmail.com>
2021-12-02 21:30:41 -0800

    2021 day 2 part 2 in racket

--- 2021/02/02.rkt
+++ 2021/02/02.rkt
@@ -31,7 +31,29 @@
           (cons (cons 'position
                       (cons (+ (extract-position dataset) amount)
                             '()))
-                '()))))
+                (cdr (cdr dataset))))))
+
+(define extract-aim
+  (lambda (dataset)
+    (car (cdr (car (cdr (cdr dataset)))))))
+
+(define add-aim
+  (lambda (dataset amount)
+    (cons (car dataset)
+          (cons (car (cdr dataset))
+                (cons (cons 'aim
+                            (cons (+ (extract-aim dataset) amount)
+                                  '()))
+                      (cdr (cdr (cdr dataset))))))))
+
+(define subtract-aim
+  (lambda (dataset amount)
+    (cons (car dataset)
+          (cons (car (cdr dataset))
+                (cons (cons 'aim
+                            (cons (- (extract-aim dataset) amount)
+                                  '()))
+                      (cdr (cdr (cdr dataset))))))))
 
 (define process
   (lambda (line dataset)
@@ -39,20 +61,21 @@
     (define direction (car line-pair))
     (define amount (string->number (car (cdr line-pair))))
     (cond
-      [(equal? direction "up") (subtract-depth dataset amount)]
-      [(equal? direction "down") (add-depth dataset amount)]
-      [(equal? direction "forward") (add-position dataset amount)]
+      [(equal? direction "up")      (subtract-aim dataset amount)]
+      [(equal? direction "down")    (add-aim dataset amount)]
+      [(equal? direction "forward") (add-depth (add-position dataset amount)
+                                               (* amount (extract-aim dataset)))]
       [else direction])))
 
 (define loop ; recurses until current-input-port is empty
   (lambda (dataset)
-    ; (newline) (print dataset) (newline)
+    ; (printf "~n~s~n" dataset)
     (let ([line (read-line (current-input-port))])
       (if (eof-object? line)
         dataset
         (loop (process line dataset))))))
 
-(let* ([initial-state '((depth 0) (position 0))]
+(let* ([initial-state '((depth 0) (position 0) (aim 0))]
        [processed     (loop initial-state)])
   (printf "~a~n" processed)
   (printf "multiplied: ~a~n" (* (extract-depth processed) (extract-position processed))))
