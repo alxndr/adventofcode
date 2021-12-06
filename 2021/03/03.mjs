99def213cabfc896960c22d220cfb2cebb652619
Alexander <alxndr+github@gmail.com>
2021-12-06 15:20:15 -0800

    2021 day 3, WIP
    this implementation is wrong

--- 2021/03/03.mjs
+++ 2021/03/03.mjs
@@ -1,29 +1,63 @@
 import {readFile} from '../helpers.file.mjs'
 
 const data = (await readFile('./input.txt')).split(/\n+/)
+// const data = (await readFile('./input-short.txt')).split(/\n+/)
+global.console.log('input length:', data.length)
 
-const mostCommonBits = data.reduce((mCB, elem, index) => {
-  const bits = elem.split('')
-  bits.forEach((bit, nthBit) => {
-    mCB[nthBit] || (mCB[nthBit] = {['0']: 0, ['1']: 0})
-    mCB[nthBit][bit] += 1
-  })
-  return mCB
-}, [])
+function filterByIthBit(bitstring, i, value) {
+  return bitstring.slice(i, i+1) === value
+}
 
-global.console.log(mostCommonBits)
+function calculateFrequencies(input, nthBit) {
+  return input.reduce((acc, row) => {
+    if (!(row?.length)) return acc
+    const bit = row.slice(nthBit, nthBit + 1)
+    return {
+      '0': bit === '0' ? acc['0']+1 : acc['0'],
+      '1': bit === '1' ? acc['1']+1 : acc['1'],
+    }
+  }, {'0': 0, '1': 0})
+} // TODO new implementation: partition the input by first-char, then let calling fn decide what to do with larger list
 
-const gamma = mostCommonBits.map((counts) => counts['0'] > counts['1'] ? '0' : '1').join('')
-const epsilon = mostCommonBits.map((counts) => counts['0'] > counts['1'] ? '1' : '0').join('')
+function filterByMostCommonBit(data, bit) {
+  if (data?.length === 1) return data
+  const bitFrequency = calculateFrequencies(data, bit)
+  return data.filter(row => filterByIthBit(row, bit, bitFrequency['0'] > bitFrequency['1'] ? '0' : '1'))
+}
 
-const gammaDec = parseInt(gamma, 2)
-const epsilonDec = parseInt(epsilon, 2)
+function filterByLeastCommonBit(data, bit) {
+  if (data?.length === 1) return data
+  const bitFrequency = calculateFrequencies(data, bit)
+  return data.filter(row => filterByIthBit(row, bit, bitFrequency['0'] < bitFrequency['1'] ? '0' : '1'))
+}
 
-global.console.log({
-  gamma,
-  gammaDec,
-  epsilon,
-  epsilonDec,
-  powerConsumption: gammaDec * epsilonDec,
-})
+function oxygen(data) {
+  const inputRecordLength = data?.[0]?.length
+  let dataset = data
+  for (
+    let bit = 0;
+    bit < inputRecordLength;
+    bit++
+  ) {
+    dataset = filterByMostCommonBit(dataset, bit)
+  }
+  return dataset
+}
 
+function carbondioxide(data) {
+  const inputRecordLength = data?.[0]?.length
+  let dataset = data
+  for (
+    let bit = 0;
+    bit < inputRecordLength;
+    bit++
+  ) {
+    dataset = filterByLeastCommonBit(dataset, bit)
+  }
+  return dataset
+}
+
+const oTwo = parseInt(oxygen(data), 2),
+  coTwo = parseInt(carbondioxide(data), 2)
+
+global.console.log({'O2': oTwo, 'CO2': coTwo, rating: oTwo * coTwo})
