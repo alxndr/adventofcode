fbb16b23dae69d623eee110bf09550c623862313
Alexander <alxndr+github@gmail.com>
2021-12-10 16:54:21 -0800

    2021 day 7, part 1, done (nodejs)

---
+++ 2021/07/07.mjs
@@ -0,0 +1,17 @@
+import {readFile} from '../helpers.file.mjs'
+const input = (await readFile('./input.txt')).split(/\n/)[0].split(',').map(Number)
+
+const sum = input.reduce((a,b) => a + b)
+global.console.log({length: input.length})
+
+function distanceFrom(n, list) {
+  return list.reduce((sum, num) => sum + Math.abs(n - num), 0)
+}
+const memo = []
+for (let i = 0; i <= input.length; i++) {
+  memo[i] = distanceFrom(i, input)
+}
+const min = Math.min(...memo)
+global.console.log({min})
+const index = memo.indexOf(min)
+global.console.log({index})
