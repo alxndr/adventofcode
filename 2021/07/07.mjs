60ec685706da7adf8b005ec15872a776232d06d3
Alexander <alxndr+github@gmail.com>
2021-12-10 17:04:35 -0800

    2021 day 7, part 2, done (nodejs)

--- 2021/07/07.mjs
+++ 2021/07/07.mjs
@@ -4,8 +4,11 @@ const input = (await readFile('./input.txt')).split(/\n/)[0].split(',').map(Numb
 const sum = input.reduce((a,b) => a + b)
 global.console.log({length: input.length})
 
+function totalStepCost(steps) {
+  return steps * (steps + 1) / 2
+}
 function distanceFrom(n, list) {
-  return list.reduce((sum, num) => sum + Math.abs(n - num), 0)
+  return list.reduce((sum, num) => sum + totalStepCost(Math.abs(num - n)), 0)
 }
 const memo = []
 for (let i = 0; i <= input.length; i++) {
