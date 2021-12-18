208bb24e2a9a28b8fbfe2f9d97b3a41e0a3abc18
Alexander <alxndr+github@gmail.com>
2021-12-18 12:38:35 -0800

    2021 day 12, part 2: WIP inefficient algorithm (nodejs)

--- 2021/12/12.mjs
+++ 2021/12/12.mjs
@@ -1,19 +1,20 @@
-// have to run using `--stack-size=4096` in order to get through the input file in NodeJS 17
+// have to run using `--stack-size=8192` in order to get through input-c file in NodeJS 17
+// ...blows the heap using the full input file
 
 import {readFile} from '../helpers.file.mjs'
-const input = (await readFile('./input.txt')).split(/\n/)
+const input = (await readFile('./input-c.txt')).split(/\n/)
 
-// "Your goal is to find the number of distinct paths that start at start, end at end, and don't visit small caves more than once."
+function isUnique(list) {
+  return new Set(list).size === list.length
+}
 
 function Cave(value) {
   this.name = value
   this.edges = []
-  // "all paths you find should visit small caves at most once, and can visit big caves any number of times"
   this.isBig = value === value.toUpperCase()
-  this.visited = false
 }
 const caves = input.reduce((caves, inputLine) => {
-  if (!inputLine.length) 
+  if (!inputLine.length)
     return caves
   const [startpoint, endpoint] = inputLine.split('-')
   const startNode = caves[startpoint] || (caves[startpoint] = new Cave(startpoint))
@@ -24,19 +25,24 @@ const caves = input.reduce((caves, inputLine) => {
 }, {})
 
 function Path(sequence) {
-  this.sequence = [...sequence] // of caves
-  this.add = (cave) => new Path([...this.sequence, {...cave, visited: true}])
-  this.lastCave = () => this.sequence.slice(-1)[0]
+  const lastCave = () => this.sequence.slice(-1)[0]
+  this.sequence = sequence
+  this.add = (cave) => new Path([...this.sequence, cave])
   this.toString = () => this.sequence.map(cave => cave.name).join(',')
+  this.isAtEnd = lastCave().name === caves.end.name
   this.isCaveEligibleToVisit = (cave) => {
     if (cave.name === caves.start.name)
       return false
-    if (cave.isBig)
+    if (cave.name === caves.end.name)
       return true
-    return !this.sequence.find(c => c.name === cave.name)
+    if (cave.isBig) // "big caves can be visited any number of times"
+      return true
+    // "a single small cave can be visited at most twice, and the remaining small caves can be visited at most once"
+    const smallCavesVisited = this.sequence.filter(c => !c.isBig).map(c => c.name)
+    return !smallCavesVisited.includes(cave.name) || isUnique(smallCavesVisited)
   }
   this.findEligibleNextCaves = () => {
-    return this.lastCave().edges.filter(this.isCaveEligibleToVisit)
+    return lastCave().edges.filter(this.isCaveEligibleToVisit)
   }
 }
 
@@ -44,22 +50,23 @@ function findPaths(paths, successfulPaths = []) {
   if (!paths.length)
     return successfulPaths
   const [path, ...restPaths] = paths
-  const cave = path.lastCave()
-  if (cave.name === caves.end.name)
+  if (path.isAtEnd)
     return findPaths(restPaths, [...successfulPaths, path])
-  return findPaths([...restPaths, ...path.findEligibleNextCaves().map(cave => path.add(cave)),], successfulPaths)
+  const morePaths = path.findEligibleNextCaves().map(cave => path.add(cave))
+  return findPaths([...restPaths, ...morePaths], successfulPaths)
 }
 
 const successful = findPaths([new Path([caves.start])])
 global.console.log('num successful paths:', successful.length)
-global.console.log('longest:', successful
-  .reduce((longest, path) => {
-    const [firstLongPath] = longest
-    if (!(firstLongPath?.sequence.length) || path.sequence.length > firstLongPath.sequence.length)
-      return [path]
-    if (path.sequence.length === firstLongPath.sequence.length) {
-      return [...longest, path]
-    return longest
-    }
-  }, [])
-  .map(p => p.toString()))
+// global.console.log(successful.map(p => p.toString()).join('\n'))
+// global.console.log('longest:', successful
+//   .reduce((longest, path) => {
+//     const [firstLongPath] = longest
+//     if (!(firstLongPath?.sequence.length) || path.sequence.length > firstLongPath.sequence.length)
+//       return [path]
+//     if (path.sequence.length === firstLongPath.sequence.length) {
+//       return [...longest, path]
+//     return longest
+//     }
+//   }, [])
+//   .map(p => p.toString()))
