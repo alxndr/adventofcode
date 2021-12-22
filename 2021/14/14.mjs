496a70aaf039c9603e77d9430700fb5ffa3087a8
Alexander <alxndr+github@gmail.com>
2021-12-22 15:31:44 -0800

    2021 day 14, part 2: done (nodejs)
    abandoned the logic to generate the template as a string each
    iteration, since the string itself or specific ordering is not
    important; the pairs are the important parts... so in this
    implementation each iteration is tracking the occurrences of each
    pair as well as the count of each letter

--- 2021/14/14.mjs
+++ 2021/14/14.mjs
@@ -1,70 +1,88 @@
-// run with `--stack-size=2048` to get through ten steps of full input file
-
 import {readFile} from '../helpers.file.mjs'
 const inputLines = (await readFile('./input.txt')).split(/\n/)
 
 // first line of inputLines is the automaton input
 // then a newline
 // then a chunk of transformation rules... which have '->' in em
+const [template, _, ...transformationRules] = inputLines
 
-const mapping = inputLines.slice(2).reduce((mapping, inputLine) => {
+const mapping = transformationRules.reduce((mapping, inputLine) => {
   if (!inputLine.length) return mapping
   const [startingSequence, newInfix] = inputLine.split(' -> ')
   return {...mapping, [startingSequence]: newInfix}
 }, {})
 
-const alphabet = Object.entries(mapping).reduce((alphabet, [pair, infix]) => {
-  [...pair.split(''), infix].forEach(letter => alphabet[letter] || (alphabet[letter] = true))
-  return alphabet
-}, {})
-
-function allPairs(string = '', foundPairs = []) {
-  if (string.length < 2)
-    return foundPairs
-  return allPairs(string.slice(1), [...foundPairs, string.slice(0, 2)])
-}
-function Automaton(input) {
-  const template = input
-  let value = template
-  const possibleLetters = value.split('').reduce((chrs, chr) => {
-    if (!chrs[chr])
-      chrs[chr] = true 
-    return chrs
-  }, alphabet)
-  let steps = 0
-  this.step = () => {
-    ++steps
-    value = allPairs(value)
-      .reduce((sequence, pair) => `${sequence}${pair[0]}${mapping[pair] || ''}`, '')
-      + value.slice(-1)
+const obj = template.split('').reduce((data, letter, index, arr) => {
+  if (data.letterCounts[letter]) { // count occurrences of letters
+    data.letterCounts[letter] += 1
+  } else {
+    data.letterCounts[letter] = 1
   }
-  this.size = () => value.length
-  this.occurrencesOf = (chr) => value.match(new RegExp(chr, 'g')).length
-  this.steps = () => Number(steps)
-  this.stats = () => {
-    global.console.log('Automaton, template:', template)
-    global.console.log('alphabet:', Object.keys(possibleLetters).sort().join(''))
-    global.console.log('steps:', this.steps(), 'size:', this.size())
-    const occurrencesSorted = Object.keys(possibleLetters).map(letter => ({letter, num: this.occurrencesOf(letter)})).sort((a, b) => a.num - b.num)
-    const leastCommon = occurrencesSorted[0]
-    const mostCommon = occurrencesSorted.slice(-1)[0]
-    global.console.log('most common:', mostCommon.letter, mostCommon.num)
-    global.console.log('least common:', leastCommon.letter, leastCommon.num)
-    global.console.log('difference:', mostCommon.num - leastCommon.num)
+  if (arr[index + 1]) {
+    const pair = `${letter}${arr[index + 1]}`
+    if (!data.pairs[pair]) data.pairs[pair] = 0
+    data.pairs[pair] += 1
+  }
+  return data
+}, {letterCounts: {}, pairs: {}})
+
+function step(obj) {
+  // obj has letterCounts and pairs
+  const {letterCounts, pairs} = obj
+  const newPairsDict = Object.entries(pairs).reduce((dict, [pair, count]) => {
+    const infix = mapping[pair]
+    if (infix) {
+      if (letterCounts[infix]) letterCounts[infix] += count
+      else                     letterCounts[infix] = count
+      const [firstLetter, secondLetter] = pair.split('')
+      const firstNewPair = `${firstLetter}${infix}`
+      const secondNewPair = `${infix}${secondLetter}`
+      if (dict[firstNewPair]) dict[firstNewPair] += count
+      else                    dict[firstNewPair] = count
+      if (dict[secondNewPair]) dict[secondNewPair] += count
+      else                     dict[secondNewPair] = count
+    } else {
+      if (dict[pair]) dict[pair] += count
+      else            dict[pair] = count
+    }
+    return dict
+  }, {})
+  return {
+    letterCounts,
+    pairs: newPairsDict,
   }
 }
 
-const automaton = new Automaton(inputLines[0])
+function multipleSteps(obj, n) { // recursive
+  logPairs(obj)
+  if (n)
+    return multipleSteps(step(obj), n - 1)
+  return obj
+}
 
-automaton.step()
-automaton.step()
-automaton.step()
-automaton.step()
-automaton.step()
-automaton.step()
-automaton.step()
-automaton.step()
-automaton.step()
-automaton.step()
+const sum = (a, b) => a + b
 
-automaton.stats()
+function logPairs({letterCounts, pairs}) {
+  global.console.log(Object.entries(pairs).map(([pair, count]) => `${pair}:${count}`).join('  '))
+  global.console.log('length: ', Object.values(pairs).reduce(sum) + 1)
+  const {
+    mostCommonCount,
+    mostCommonLetter,
+    leastCommonCount,
+    leastCommonLetter,
+  } = Object.entries(letterCounts).reduce((result, [letter, count]) => {
+    if (count > result.mostCommonCount) {
+      result.mostCommonCount = count
+      result.mostCommonLetter = letter
+    }
+    if (count < result.leastCommonCount) {
+      result.leastCommonCount = count
+      result.leastCommonLetter = letter
+    }
+    return result
+  }, {mostCommonCount: 0, leastCommonCount: Infinity})
+  global.console.log({mostCommonLetter, mostCommonCount, leastCommonLetter, leastCommonCount})
+  global.console.log('difference:', mostCommonCount - leastCommonCount)
+}
+
+multipleSteps(obj, 40)
