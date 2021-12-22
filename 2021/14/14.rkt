13868f56a0a5b0ae62cdabf78944ff137c14c181
Alexander <alxndr+github@gmail.com>
2021-12-21 19:42:12 -0800

    2021 day 14, part 1: refactor (racket)
    using `racket/base`

--- 2021/14/14.rkt
+++ 2021/14/14.rkt
@@ -1,20 +1,16 @@
-#lang racket
+#lang racket/base
 
 (define build-mapping
   (lambda (mapping input)
     (let ([line (read-line (input))])
       (if (eof-object? line)
         mapping
-        (let* ([split (string-split line " -> ")]
-               [pair (car split)]
-               [infix (car (cdr split))])
-          ; (printf "add to mapping... '~a' produces a '~a'~n" pair infix)
+        (let* ([pair (substring line 0 2)]
+               [infix (substring line 6 7)])
           (build-mapping (cons (cons pair (cons infix '())) mapping) input))))))
-
 (define input-to-mapping
   (lambda (input-port)
     (build-mapping '() input-port)))
-
 (define look-up
   (lambda (pair mapping)
     (if (null? mapping)
@@ -26,7 +22,7 @@
           first-infix
           (look-up pair (cdr mapping)))))))
 
-(define step-r
+(define step-r ; TODO reimplement without using strings
   (lambda (template mapping result)
     (if (eq? 1 (string-length template))
       ; at the last char of input string...
@@ -48,7 +44,10 @@
     (step-r template mapping "")))
 (define step-n-times
   (lambda (n template mapping)
-    ; (printf "steppin ~a times... ~a~n" n template)
+    (let ([len (string-length template)])
+      (if (> len 9e4)
+        (printf "remaining iterations: ~a \t string length: ~a~n" n (string-length template))
+        #f))
     (if (eq? n 0)
       template
       (step-n-times (- n 1) (step template mapping) mapping))))
@@ -65,46 +64,39 @@
                          (hash-set hashtable first-letter 1)))))))
 (define find-most-and-least-common
   (lambda (letter-occurrences)
-    (foldl (lambda (pair data)
-             (let ([letter (car pair)]
-                   [count (cdr pair)])
-               (if (eq? '() data) ; on the first iteration; letter & count are both most and least common
-                 (quasiquote (((unquote count) . (unquote letter))
-                              .
-                              ((unquote count) . (unquote letter))))
-                 (let ([most-common-letter (car data)]
-                       [least-common-letter (cdr data)])
-                   (cond
-                     ((> count (car most-common-letter))
-                      (quasiquote (((unquote count) . (unquote letter))
-                                   .
-                                   (unquote least-common-letter))))
-                     ((< count (car least-common-letter))
-                      (quasiquote ((unquote most-common-letter)
-                                   .
-                                   ((unquote count) . (unquote letter)))))
-                     ('else data))))))
-           '()
-           (hash->list letter-occurrences))))
+    (foldl
+      (lambda (pair data)
+        (let ([letter (car pair)]
+              [count (cdr pair)])
+          (if (eq? '() data) ; on the first iteration; letter & count are both most and least common
+            (quasiquote (((unquote count) . (unquote letter)) . ((unquote count) . (unquote letter))))
+            (let ([most-common-letter (car data)]
+                  [least-common-letter (cdr data)])
+              (cond
+                ((> count (car most-common-letter))
+                 (quasiquote (((unquote count) . (unquote letter)) . (unquote least-common-letter))))
+                ((< count (car least-common-letter))
+                 (quasiquote ((unquote most-common-letter) . ((unquote count) . (unquote letter)))))
+                ('else data))))))
+      '()
+      (hash->list letter-occurrences))))
 (define print-stats
   (lambda (str)
-    (let* ([letter-occurrences (count-letters str (hash))] ; TODO try using `make-hash` ?
+    (printf "Polymer length: ~a~n" (string-length str))
+    (let* ([letter-occurrences (count-letters str (hash))] ; `hash` is the immutable one
            [common (find-most-and-least-common letter-occurrences)]
            [most-common (car common)]
            [least-common (cdr common)])
-      (printf "Polymer length: ~a~n" (string-length str))
       (printf "Most Common Letter: ~a (~a)~n" (cdr most-common) (car most-common))
       (printf "Least Common Letter: ~a (~a)~n" (cdr least-common) (car least-common))
       (printf "(difference: ~a)~n" (- (car most-common) (car least-common)))
-    )
-))
+    )))
 
 (let* ([template (read-line (current-input-port))] ; first line is automaton starting input
        [_        (read-line (current-input-port))] ; second line is blank
        [mapping  (input-to-mapping current-input-port)]) ; rest of input are rules to create a mapping
   (printf "mapping ~a~n" mapping)
-  (printf "template ~a~n" template)
-  (let* ([after-ten-steps (step-n-times 10 template mapping)])
-    (printf "after ten steps...~n")
-    (print-stats after-ten-steps)
-))
+  (time
+    (print-stats (time
+                   (step-n-times 10 template mapping)))))
+; multi-minute results for more than 17 iterations...
