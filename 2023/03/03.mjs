77681b960b79a62ae4bcd4ad08d200ef147df20b
Alexander <alxndr+github@gmail.com>
2023-12-03 12:03:33 -0800

    2023 day 3 part 2: done (JS)

--- 2023/03/03.mjs
+++ 2023/03/03.mjs
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 import {readFileSync} from 'fs'
-const input = (await readFileSync('sample.txt', 'utf-8'))
+const input = (await readFileSync('input.txt', 'utf-8'))
+  .replace(/\./g, " ") // use spaces rather than periods...
   .split(/\r?\n/)
 if (!input[input.length-1].length) input.pop() // trim a POSIX trailing newline
 
@@ -11,58 +12,59 @@ if (!input[input.length-1].length) input.pop() // trim a POSIX trailing newline
 const lines = input;
 const RE_ASTERISK = /\*/g
 const RE_NUMBER = /\d/
-const RE_NUMBERS_AT_BEGINNING = /^\d+/
-const RE_NUMBERS_AT_END = /\d+$/
-const RE_NUMBERS_IN_SEQUENCE_OF_THREE = /^([\d.])([\d.])([\d.])$/ // relies on assumptions about input...
-const RE_NUMBERS_SEPARATED_BY_PERIODS = /(\d+)\.+(\d+)/ // relies on assumptions about input...
 const sum = lines.reduce((acc, line, lineIndex) => {
   let match;
   while (match = RE_ASTERISK.exec(line)) {
-    const {index} = match;
-    const adjacentNumbers = [];
+    const starIndex = match.index;
+    const adjacentNumbers = []
 
-    const strBeforeMatch = line.slice(0, index)
-    const numberBefore = strBeforeMatch.match(RE_NUMBERS_AT_END)?.[0]
-    if (numberBefore)
-      adjacentNumbers.push(Number(numberBefore))
-
-    const strAfterMatch = line.slice(index + 1)
-    const numberAfter = strAfterMatch.match(RE_NUMBERS_AT_BEGINNING)?.[0]
-    if (numberAfter)
-      adjacentNumbers.push(Number(numberAfter))
-
-    if (lineIndex > 0) { // check above
-      const lineAbove = lines[lineIndex-1];
-      const aboveSequence = lineAbove.slice(index - 1, index + 2) // TODO account for L and R edges...
-      global.console.log({index, lineIndex, aboveSequence})
-      // there are 8 optns for above (and below):
-        // .#. = push
-        // ##. = grab 1 left
-        // .## = grab 1 right
-        // ### = push
-      if (aboveSequence === '...') {
-        // ... = next
-        'no-op';
+    if (lineIndex > 0) {
+      const lineAbove = lines[lineIndex-1]
+      const charAboveAsterisk = lineAbove[starIndex]
+      if (charAboveAsterisk === ' ') {
+        const beforeAsterisk = lineAbove.substring(starIndex-3, starIndex)
+        const beforeMatch = beforeAsterisk.match(/(\d+)$/)
+        if (beforeMatch) {adjacentNumbers.push(beforeMatch[1])}
+        const afterAsterisk = lineAbove.substring(starIndex+1, starIndex+4)
+        const afterMatch = afterAsterisk.match(/^(\d+)/)
+        if (afterMatch) {adjacentNumbers.push(afterMatch[1])}
       } else {
-        global.console.log({aboveSequence})
-        const [_, a, b, c] = aboveSequence.match(RE_NUMBERS_IN_SEQUENCE_OF_THREE)
-        if (b === '.') {
-          // #.# = grab 2 left thru 2 right
-          // #.. = grab 2 left
-          // ..# = grab 2 right
-          const numberMatches = lineAbove.slice(index - 3, index + 4).match(RE_NUMBERS_SEPARATED_BY_PERIODS)
-          global.console.log({numberMatches})
-          adjacentNumbers.concat(numberMatches)
-        }
+        // char Above is a number...
+        const aboveMatch = lineAbove.substring(starIndex-2, starIndex+3).match(/.?.?\b(\d+)\b.?.?/)?.[1]
+        if (aboveMatch) adjacentNumbers.push(aboveMatch)
+      }
+
+    }
+
+    const middleLine = line.substring(starIndex-3, starIndex+4);
+    const beforeMatch = middleLine.match(/(\d+)\*/)
+    if (beforeMatch) adjacentNumbers.push(beforeMatch[1])
+    const afterMatch = middleLine.match(/\*(\d+)/)
+    if (afterMatch) adjacentNumbers.push(afterMatch[1])
+
+    if (lineIndex < lines.length - 1) {
+      const lineBelow = lines[lineIndex+1]
+      const charBelowAsterisk = lineBelow[starIndex]
+      if (charBelowAsterisk === ' ') {
+        const beforeAsterisk = lineBelow.substring(starIndex-3, starIndex)
+        const beforeMatch = beforeAsterisk.match(/(\d+)$/)
+        if (beforeMatch) {adjacentNumbers.push(beforeMatch[1])}
+        const afterAsterisk = lineBelow.substring(starIndex+1, starIndex+4)
+        const afterMatch = afterAsterisk.match(/^(\d+)/)
+        if (afterMatch) {adjacentNumbers.push(afterMatch[1])}
+      } else {
+        // char below is a number...
+        const belowMatch = lineBelow.substring(starIndex-2, starIndex+3).match(/.?.?\b(\d+)\b.?.?/)?.[1]
+        if (belowMatch) adjacentNumbers.push(belowMatch)
       }
     }
-    if (lines[lineIndex+1]) { // check below
-    }
 
-    if (adjacentNumbers.length) {
-      global.console.log('* (%d,%d) adjacent:', index, lineIndex, adjacentNumbers)
-    } else
-      global.console.log('* (%d,%d)', index, lineIndex)
+    global.console.log('(%d,%d): %s', starIndex, lineIndex, adjacentNumbers.join(','))
+
+    if (adjacentNumbers.length === 2) {
+      const gearRatio = Number(adjacentNumbers[0]) * Number(adjacentNumbers[1])
+      acc += gearRatio;
+    }
   }
   return acc
 
