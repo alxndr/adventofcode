1eb6474c466a7675fd7b3e96e291b7f905f39e1b
Alexander <alxndr+github@gmail.com>
2023-12-03 22:57:05 -0800

    2023 day 4: part 2 done (JS)

--- 2023/04/04.mjs
+++ 2023/04/04.mjs
@@ -7,22 +7,23 @@ if (!input[input.length-1].length) input.pop() // trim a POSIX trailing newline
 const RE_SPACES = /\s+/
 
 const numMatches = {}
+const numCards = Array(input.length).fill(1)
 
-global.console.log(
-  'result? ', input.reduce((acc, elem, index) => {
-    numMatches[index] = 0
-    const [_, card] = elem.split(/:\s+/)
-    const [winners, hand] = card.split(' | ')
-    const winnersArr = winners.split(RE_SPACES)
-    const handArr = hand.split(RE_SPACES)
-    handArr.map(handNumStr => {
-      if (winnersArr.includes(handNumStr)) {
-        numMatches[index]++
-      }
-    })
-    if (numMatches[index]) {
-      return acc + (2 ** (numMatches[index] - 1));
+input.map((line, index) => {
+  numMatches[index] = 0
+  const [_, card] = line.split(/:\s+/)
+  const [winners, hand] = card.split(' | ')
+  const winnersArr = winners.split(RE_SPACES)
+  const handArr = hand.split(RE_SPACES)
+  handArr.map(handNumStr => {
+    if (winnersArr.includes(handNumStr)) {
+      numMatches[index]++
     }
-    return acc
-  }, 0)
-)
+  })
+  for (let i = 1; i <= numMatches[index]; i++) {
+    global.console.log('there are %d matches... and we have %d of this card...', numMatches[index], numCards[index])
+    numCards[index+i] = numCards[index+i] + numCards[index]
+  }
+  global.console.log('idx:%d (%d) \t %s', index, numCards[index], numCards.slice(index, index+5))
+})
+global.console.log(numCards.reduce((sum, n) => sum + n))
