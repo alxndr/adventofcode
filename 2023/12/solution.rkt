8c5dd965c22f2f5c351d2a02fbb7db5ab45d6ba0
Alexander <alxndr+github@gmail.com>
2023-12-14 00:18:07 -0800

    2023 day 14: part 1 done (JS)

--- 2023/12/solution.rkt
+++ 2023/12/solution.rkt
@@ -1,33 +1,28 @@
 #lang racket
 
 (provide solve-part1
-         check-springcode?*
+         check-springcode?
          generate-possibilities
-         string-to-chars)
+         string-to-chars
+         solve-part2
+         split-up-springspec)
 
-(require "input.rkt")
+(require "input.rkt"
+         graph)
 
-(printf "\n\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n\n\n")
-
-(define (check-springcode? springspec-string checks-string)
-  (define split-up (string-split springspec-string "." #:repeat? #t))
-  (define checks (map string->number (string-split checks-string ",")))
-  (equal? checks (map string-length split-up)))
-
-(define (check-springcode?* springspec-string checks-list)
-  (define split-up (string-split springspec-string "." #:repeat? #t))
-  (equal? checks-list (map string-length split-up)))
+(define (split-up-springspec springspec-string)
+  (string-split springspec-string "." #:repeat? #t))
+(define (check-springcode? springspec-string checks-list)
+  (equal? checks-list (map string-length (split-up-springspec springspec-string))))
 
 (define (string-to-chars s)
   (reverse (list-tail (reverse (list-tail (string-split s "") 1)) 1)))
 
 (define (generate-possibilities_ cypher possibilities)
-  ; TODO generate a TREE rather than a big list... then we can prune to reduce search space
   (if (empty? cypher)
-    possibilities ; TODO use of append means we don't have to reverse here right?
-    (let [[top-letter (car cypher)]
-          ]
-      (generate-possibilities_ (cdr cypher) ; TODO prune as we generate..??
+    possibilities
+    (let [[top-letter (car cypher)]]
+      (generate-possibilities_ (cdr cypher)
                                (if (equal? "?" top-letter)
                                  (flatten (map (λ (possibility) (list
                                                                    (string-append possibility ".")
@@ -43,7 +38,7 @@
   (define cypher (string-to-chars (car line-tuple)))
   (define counts (map string->number (string-split (car (cdr line-tuple)) ",")))
   (define possibilities-for-line (generate-possibilities cypher))
-  (filter (λ (possibility) (check-springcode?* possibility counts))
+  (filter (λ (possibility) (check-springcode? possibility counts))
           possibilities-for-line)
   )
 
@@ -51,4 +46,50 @@
   (apply +
          (map (λ (input-line) (length (possibilities-per-line (string-split input-line))))
               split-input))
+  )
+
+(define (list-these-things what how-many)
+  (build-list how-many (lambda (_) what)))
+
+(define (process-cypher cypher fodder)
+  (printf "\n\npossibilities-pruned ... ~a \n fodder: ~v \n" cypher fodder)
+  ; wanna depth-first-ly create options from cypher and test w predicate
+  (if (empty? cypher)
+    (reverse fodder) ; TODO this is generating a list of possibilities ... can it just be a count we iterate...
+    (let [[next-char (car cypher)]]
+      ; if we have a # or .,
+      ; ...stick it on to things (WHAT?) and check against predicate, keep if passes
+      ; if not...
+      ; ... it's shrödingers char... '("#" ".")
+      (process-cypher (cdr cypher)
+                            (cons (match next-char ["?" (list "#" ".")]
+                                                   [  s              s])
+                                  fodder)
+                            ))))
+
+(define (is-viable-spec? spec)
+  (printf "predicate called here... ~v \n" potential-spec)
+  ; TODO change this fn to ... _get_ springcode for potential spec, and then...
+  ;   if its length is > (length big-counts), it's no good
+  ;   if its nth item is > big-counts nth item...
+  ;   ...but otherwise it's still valid fodder
+)
+
+(define (possibilies-per-line-part2 line-tuple)
+  (define cypher-raw (string-to-chars (car line-tuple)))
+  (define big-cypher (flatten (add-between (build-list 5 (lambda (_) cypher-raw)) "?")))
+  (define counts-raw (map string->number (string-split (car (cdr line-tuple)) ",")))
+  (define big-counts (flatten (list-these-things counts-raw 5)))
+  (printf "big-cypher is ~s chars long \n" (length big-cypher))
+  (define spec (process-cypher big-cypher '()))
+  (printf "poss !! ") (pretty-print poss)
+  (length poss)
+  )
+
+(define (solve-part2 split-input)
+  (apply +
+         (map (λ (input-line) (length (possibilies-per-line-part2 (string-split input-line))))
+              ;; split-input
+              (list (car split-input))
+              ))
   )
--- 2023/12/test.rkt
+++ 2023/12/test.rkt
@@ -6,28 +6,38 @@
          rackunit/text-ui)
 
 (run-tests
-
   (test-suite
     "2023 day 12"
 
-    (test-suite
-      "string-to-chars"
-      (test-case "basic" (check-equal?
-                           (string-to-chars "foo")
-                           '("f" "o" "o"))))
+
+    (test-suite "string-to-chars"
+      (test-case "basic" (check-equal? (string-to-chars "foo") '("f" "o" "o"))))
+
+
+    (test-suite "split-up-springspec"
+      (test-case "fundamentals"
+         (check-equal? (split-up-springspec ".") '())
+         (check-equal? (split-up-springspec "#") '("#")))
+      (test-case "basics"
+         (check-equal? (split-up-springspec "#.#") '("#" "#"))
+         (check-equal? (split-up-springspec ".##..###.####.#") '("##" "###" "####" "#")))
+      )
+
 
     (test-suite "check-springcode"
-      (test-case "basic truthy" (check-equal? (check-springcode?* "#" '(1)) #t))
-      (test-case "basic falsy" (check-equal? (check-springcode?* "." '(1)) #f))
-      (test-case "medium truthy" (check-equal? (check-springcode?* "#.#.###" '(1 1 3)) #t))
-      (test-case "medium falsy" (check-equal? (check-springcode?* "#.#.###" '(1 1 4)) #f))
-      (test-case "longer truthy" (check-equal? (check-springcode?* ".#.###.#.######" '(1 3 1 6)) #t))
-      (test-case "longer falsy" (check-equal? (check-springcode?* "##.####.#.###.#.######" '(1 3 1 6)) #f))
+      (test-case "basic truthy" (check-equal? (check-springcode? "#" '(1)) #t))
+      (test-case "basic falsy" (check-equal? (check-springcode? "." '(1)) #f))
+      (test-case "medium truthy" (check-equal? (check-springcode? "#.#.###" '(1 1 3)) #t))
+      (test-case "medium falsy" (check-equal? (check-springcode? "#.#.###" '(1 1 4)) #f))
+      (test-case "longer truthy" (check-equal? (check-springcode? ".#.###.#.######" '(1 3 1 6)) #t))
+      (test-case "longer falsy" (check-equal? (check-springcode? "##.####.#.###.#.######" '(1 3 1 6)) #f))
       )
+
 
     (test-suite "string-to-chars"
       (test-case "basic" (check-equal? (string-to-chars "#?") '( "#" "?" )))
       )
+
 
     (test-suite "generate-possibilities"
       (test-case "basic" (check-equal? (generate-possibilities (string-to-chars "#?")) '("#." "##")))
@@ -36,23 +46,72 @@
       (test-case "longer" (check-equal? (length (generate-possibilities (string-to-chars "#.?#.?.#?#?#?.?#.##?????"))) 2048))
       )
 
+
     (test-suite
-      "solution"
+      "part 1 solution"
+      (test-case "sample" (check-equal? (solve-part1 (sample-input)) 21))
+      ;(test-case "full" (check-equal? (solve-part1 (full-input)) 7118))
+      )
+
+
+    (test-suite
+      "graph stuff..."
 
       (test-case
-        "sample"
-        (check-equal?
-          (solve-part1 (sample-input))
-          21))
-
-      (test-case
-        "full"
-        (check-equal?
-          (solve-part1 (full-input))
-          7118))
+        "something"
+        #| (check-equal? 'a
+                      'b) |#
+        )
 
       )
 
-    )
 
+    (test-suite
+      "part 2 solution"
+
+      (test-case
+        "real small sample"
+        (check-equal?
+          (solve-part2 (list (car (sample-input))))
+          1)
+        )
+
+      #| (test-case
+        "next smallest sample"
+        (check-equal?
+          (solve-part2 (list (car (sample-input))
+                             (cadr (sample-input))))
+          2)
+        ) |#
+
+      #| (test-case
+        "next smallest sample"
+        (check-equal?
+          (solve-part2 (list (car (sample-input))
+                             (cadr (sample-input))
+                             (caddr (sample-input))))
+          16386)
+        ) |#
+
+      #| (test-case
+        "sample"
+        (check-equal?
+          (solve-part2 (sample-input))
+          525152)
+        ) |#
+
+      #| (test-case
+        "full"
+        (check-equal?
+          (solve-part2 (full-input))
+          'unknown)
+        ) |#
+
+      )
+
+
+
+
+
+    )
   )
