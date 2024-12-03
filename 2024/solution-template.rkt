62491516ec891448046e5d991f6cd83ab13c0870
Alexander <alxndr+github@gmail.com>
2024-12-03 10:02:54 -0800

    tweak Racket template

--- 2024/solution-template.rkt
+++ 2024/solution-template.rkt
@@ -1,68 +1,35 @@
 #lang racket
 
-; "collection not found for module path: rackunit collection: "rackunit" in collection directories"
-; ...run the below in CLI:
-; $ raco pkg install rackunit
-
-(require racket/pretty
-         rackunit
-         rackunit/text-ui)
+(require "../advent-of-code-utils.rkt" rackunit rackunit/text-ui)
 
 (define (solve-part1 input)
-  '())
+  'TODO)
 
 (define (solve-part2 input)
-  '())
-
-(define (extract-and-split file-contents)
-  (map (λ (line)
-         (string-split line))
-       file-contents))
-
-(define (sample-input) (file->lines "sample.txt"))
-(define (full-input)   (file->lines "input.txt"))
+  'TODO)
 
 (run-tests
-  (test-suite "2024 day ##"
-
-    (test-suite "extract-and-split"
-      (test-case "basic"
-        (let ([split-input (extract-and-split (sample-input))])
-          (check-equal?
-            (car split-input)
-            '())
-          (check-equal?
-            (length split-input)
-            0)
-          )
-        )
-      )
+  (test-suite "2024 day #"
 
     (test-suite "part 1"
       (test-case "sample input"
         (check-equal?
           (solve-part1 (extract-and-split (sample-input)))
-          #f)
-        )
+          'TODO))
       #; (test-case "full input"
         (check-equal?
           (solve-part1 (extract-and-split (full-input)))
-          #f)
-        )
-      )
+          'TODO)))
 
-    (test-suite "part 2"
-      #; (test-case "sample input"
+    #; (test-suite "part 2"
+      (test-case "sample input"
         (check-equal?
           (solve-part2 (extract-and-split (sample-input)))
-          #f)
-        )
-      #; (test-case "full input"
+          'TODO))
+      (test-case "full input"
         (check-equal?
           (solve-part2 (extract-and-split (full-input)))
-          #f)
-        )
-      )
+          'TODO)))
 
-    )
   )
+)
