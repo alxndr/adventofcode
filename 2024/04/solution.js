84c65ce54b919431a372b220218922fd1a788d04
Alexander <alxndr+github@gmail.com>
2024-12-04 19:09:42 -0800

    2024 day 4: part 2 done (JS)

--- 2024/04/solution.js
+++ 2024/04/solution.js
@@ -1,21 +1,21 @@
 import fs from 'fs'
 
 const inputToMatrix = (input) => {
-  return input.split(/\r?\n/).map(s => s.trim())
-}
-
-const DIRS = {
-  N : [{       y: -1}, {       y: -2}, {       y: -3}],
-  NE: [{x:  1, y: -1}, {x:  2, y: -2}, {x:  3, y: -3}],
-  E : [{x:  1       }, {x:  2       }, {x:  3       }],
-  SE: [{x:  1, y:  1}, {x:  2, y:  2}, {x:  3, y:  3}],
-  S : [{       y:  1}, {       y:  2}, {       y:  3}],
-  SW: [{x: -1, y:  1}, {x: -2, y:  2}, {x: -3, y:  3}],
-  W : [{x: -1       }, {x: -2       }, {x: -3       }],
-  NW: [{x: -1, y: -1}, {x: -2, y: -2}, {x: -3, y: -3}],
+  return input.split(/\r?\n/).map(s => s.trim()).filter(s => s.length)
 }
 
 const part1 = () => {
+  const DIRS = {
+    N : [{       y: -1}, {       y: -2}, {       y: -3}],
+    NE: [{x:  1, y: -1}, {x:  2, y: -2}, {x:  3, y: -3}],
+    E : [{x:  1       }, {x:  2       }, {x:  3       }],
+    SE: [{x:  1, y:  1}, {x:  2, y:  2}, {x:  3, y:  3}],
+    S : [{       y:  1}, {       y:  2}, {       y:  3}],
+    SW: [{x: -1, y:  1}, {x: -2, y:  2}, {x: -3, y:  3}],
+    W : [{x: -1       }, {x: -2       }, {x: -3       }],
+    NW: [{x: -1, y: -1}, {x: -2, y: -2}, {x: -3, y: -3}],
+  }
+  console.log('part 1....\n')
   const solve = (matrix) => {
     let found = 0
     const height = matrix.length
@@ -59,16 +59,16 @@ const part1 = () => {
   console.log('sample 1:', solve(inputToMatrix(sample1))) // answer to this is 4
 
   const sample2 = `
-  MMMSXXMASM
-  MSAMXMSMSA
-  AMXSXMAAMM
-  MSAMASMSMX
-  XMASAMXAMM
-  XXAMMXXAMA
-  SMSMSASXSS
-  SAXAMASAAA
-  MAMMMXMMMM
-  MXMXAXMASX
+    MMMSXXMASM
+    MSAMXMSMSA
+    AMXSXMAAMM
+    MSAMASMSMX
+    XMASAMXAMM
+    XXAMMXXAMA
+    SMSMSASXSS
+    SAXAMASAAA
+    MAMMMXMMMM
+    MXMXAXMASX
   `
   console.log('sample 2:', solve(inputToMatrix(sample2))) // answer to this is 18
 
@@ -78,3 +78,57 @@ const part1 = () => {
 }
 
 part1()
+
+const part2 = () => {
+  console.log('\n\npart 2....\n')
+  const solve = (matrix) => {
+    const height = matrix.length
+    const width = matrix[0].length
+    const charNW = ({x,y}) => matrix[y-1][x-1]
+    const charNE = ({x,y}) => matrix[y-1][x+1]
+    const charSE = ({x,y}) => matrix[y+1][x+1]
+    const charSW = ({x,y}) => matrix[y+1][x-1]
+    const isAnX = ({x,y}) => {
+      if (matrix[y][x] != 'A')
+        return false
+      if (y === 0 || y === height - 1 || x === 0 || x === width - 1)
+        return false
+      if ( ((charNW({x,y}) === 'M' && charSE({x,y}) === 'S') || (charSE({x,y}) === 'M' && charNW({x,y}) === 'S'))
+        && ((charNE({x,y}) === 'M' && charSW({x,y}) === 'S') || (charSW({x,y}) === 'M' && charNE({x,y}) === 'S'))
+      )
+        return true
+      return false
+    }
+    let count = 0
+    for (let y = 1; y < height - 1; y++)
+      for (let x = 1; x < width - 1; x++)
+        if (isAnX({x,y}))
+          count += 1
+    return count
+  }
+
+  const sample1 = `
+    M.S
+    SAM
+    M.S
+  `
+  console.log('sample 1:', solve(inputToMatrix(sample1))) // answer to this is 1
+
+  const sample2 = `
+    .M.S......
+    ..A..MSMS.
+    .M.S.MAA..
+    ..A.ASMSM.
+    .M.S.M....
+    ......A...
+    S.S.S.SSS.
+    .A.A.A.A..
+    M.M.M.M.M.
+    .......... `
+  console.log('sample 2:', solve(inputToMatrix(sample2))) // answer to this is 9
+
+  const fullInput = fs.readFileSync('./input.txt', 'utf8')
+  console.log('full input:', solve(inputToMatrix(fullInput)))
+}
+
+part2()
