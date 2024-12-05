9acde554a2acb60d379f3ff521881827954b491c
Alexander <alxndr+github@gmail.com>
2024-12-05 00:29:15 -0800

    2024 day 5: part 2 done (JS)

--- 2024/05/solution.js
+++ 2024/05/solution.js
@@ -4,6 +4,33 @@ import {expect, test} from 'bun:test'
 const testInput = fs.readFileSync('./sample.txt', 'utf8')
 const fullInput = fs.readFileSync('./input.txt', 'utf8')
 
+function part1(input) {
+  const {rulespecs, updates} = processInput(input)
+  const correctlyOrderedUpdates = updates.filter(isCorrectlyOrdered({rulespecs, updates}))
+  return correctlyOrderedUpdates
+    .map(middleNumber)
+    .reduce((a, b) => a + b)
+}
+
+function part2(input) {
+  const {rulespecs, updates} = processInput(input)
+  const incorrectlyOrderedUpdates = updates.filter(update => !(isCorrectlyOrdered({rulespecs, updates})(update)))
+  const reordered = incorrectlyOrderedUpdates
+    .map(incorrectUpdate => {
+      return incorrectUpdate.toSorted((a, b) => {
+        const rulesA = rulespecs[a]
+        if (rulesA.mustComeBefore.includes(b))
+          return -1
+        if (rulesA.mustComeAfter.includes(b))
+          return 1
+        return 0
+      })
+    })
+  return reordered
+    .map(middleNumber)
+    .reduce((a, b) => a + b)
+}
+
 function createRulesHash(rules, ruleRaw) {
   const [earlier, later] = ruleRaw.split('|').map(Number)
   if (rules[earlier]) {
@@ -19,34 +46,43 @@ function createRulesHash(rules, ruleRaw) {
   return rules
 }
 
-function part1(input) {
+function processInput(input) {
   const inputSplit = input.trim().split('\n')
   const blankLineIndex = inputSplit.indexOf('')
   const rulespecs = inputSplit.slice(0, blankLineIndex).reduce(createRulesHash, {})
   const updates = inputSplit.slice(blankLineIndex + 1).map(u => u.split(',').map(Number))
-  const correctlyOrderedUpdates = updates
-    .filter(update => {
-      for (let i = 0; i < update.length; i++) {
-        const num = update[i]
-        const rulesForThisNumber = rulespecs[num]
-        if (!rulesForThisNumber)
-          return true // no rules about the number means it's okay
-        const prior = update.slice(0, i)
-        const following = update.slice(i + 1)
-        const {mustComeBefore, mustComeAfter} = rulesForThisNumber
-        if (prior.find(priorNum => mustComeBefore.includes(priorNum)))
-          return false
-        if (following.find(followingNum => mustComeAfter.includes(followingNum)))
-          return false
-      }
-      return true
-    })
-  return correctlyOrderedUpdates
-    .map(update => update[Math.floor(update.length / 2)])
-    .reduce((a, b) => a + b)
+  return {rulespecs, updates}
+}
+
+function isCorrectlyOrdered({rulespecs, updates}) {
+  return function(update) {
+    for (let i = 0; i < update.length; i++) {
+      const num = update[i]
+      const rulesForThisNumber = rulespecs[num]
+      if (!rulesForThisNumber)
+        return true // no rules about the number means it's okay
+      const prior = update.slice(0, i)
+      const following = update.slice(i + 1)
+      const {mustComeBefore, mustComeAfter} = rulesForThisNumber
+      if (prior.find(priorNum => mustComeBefore.includes(priorNum)))
+        return false
+      if (following.find(followingNum => mustComeAfter.includes(followingNum)))
+        return false
+    }
+    return true
+  }
+}
+
+function middleNumber(update) {
+  return update[Math.floor(update.length / 2)]
 }
 
 test('part 1', () => {
   expect(part1(testInput)).toBe(143)
   expect(part1(fullInput)).toBe(6034)
+})
+
+test('part 2', () => {
+  expect(part2(testInput)).toBe(123)
+  expect(part2(fullInput)).toBe(6305)
 })
