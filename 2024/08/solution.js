7a508db000210ec9b03f2c64dd93933c81f06950
Alexander <alxndr+github@gmail.com>
2024-12-11 14:13:24 -0800

    2024 day 8: part 2 atrocious but working (JS)
    note to self: don't ever do math in JS again

--- 2024/08/solution.js
+++ 2024/08/solution.js
@@ -25,15 +25,14 @@ function processInput(input) {
   }
 }
 
-function generatePotentialAntinodes(nodeA, nodeB) {
-  // "an antinode occurs at any point that is perfectly in line with two antennas of the same frequency - but only when one of the antennas is twice as far away as the other"
-  return [
-    {x: nodeA.x + (nodeA.x - nodeB.x), y: nodeA.y + (nodeA.y - nodeB.y)},
-    {x: nodeB.x + (nodeB.x - nodeA.x), y: nodeB.y + (nodeB.y - nodeA.y)},
-  ] // note that these need to be filtered to remove ones that are off-map
-}
-
 function part1(input) {
+  function generatePotentialAntinodes(nodeA, nodeB) {
+    // "an antinode occurs at any point that is perfectly in line with two antennas of the same frequency - but only when one of the antennas is twice as far away as the other"
+    return [
+      {x: nodeA.x + (nodeA.x - nodeB.x), y: nodeA.y + (nodeA.y - nodeB.y)},
+      {x: nodeB.x + (nodeB.x - nodeA.x), y: nodeB.y + (nodeB.y - nodeA.y)},
+    ] // note that these need to be filtered to remove ones that are off-map
+  }
   const {
     matrix,
     matrixWidth,
@@ -41,11 +40,10 @@ function part1(input) {
     antennas,
   } = processInput(input)
   const antinodes = Array(matrixHeight).fill(null).map(() => Array(matrixWidth).fill(null).map(() => 0))
-  let antinodesCount = 0
   Object.entries(antennas).forEach(([frequency, antennasAtFrequency]) => {
     if (antennasAtFrequency.length < 2) return // need at least 2 nodes
     const antinodesForFrequency = antennasAtFrequency.reduce((acc, elem, index) => {
-      if (!antennasAtFrequency[index + 1]) return acc // last antenna in the last
+      if (!antennasAtFrequency[index + 1]) return acc // last antenna in the list
       for (let cursor = index + 1; cursor < antennasAtFrequency.length; cursor++) {
         // O(n*log(n))
         const antinodesOfThisPair = generatePotentialAntinodes(elem, antennasAtFrequency[cursor])
@@ -57,9 +55,7 @@ function part1(input) {
       }
       return acc
     }, [])
-    // console.log(`antinodes @ freq:${frequency}`, antinodesForFrequency)
   })
-  console.log(antinodes.map(line => line.join('')).join('\n'))
   return antinodes.reduce((totalSum, rowOfAntinodes) => {
     return totalSum + rowOfAntinodes.reduce((rowSum, countOfFrequencies) => {
       if (countOfFrequencies)
@@ -69,7 +65,115 @@ function part1(input) {
   }, 0)
 }
 
+const delay = ms => new Promise(resolve => setTimeout(resolve, ms))
+
+const quoteUnquotePrecision = 10e12
+function fudgeItToAccountForFloatingPointNonsense(n) {
+  // e.g. 27.999999999999996 should be 28
+  return Math.round(n * quoteUnquotePrecision) / quoteUnquotePrecision
+}
+
+function part2(input) {
+  const {
+    matrix,
+    matrixWidth,
+    matrixHeight,
+    antennas,
+  } = processInput(input)
+  // console.log('\n\n' + Array(matrixWidth).fill('=').join(''))
+  // console.log(matrix.map(l => l.map(n => n === '.' ? ' ' : n).join('')).join('\n'))
+  function slope(nodeA, nodeB) { // NOTE oof I hope JS floating-point math doesn't bite me in the ass here
+    // y = m*x + b
+    // m = rise/run = (y2 - y1)/(x2 - x1)
+    // ...note when visualizing this: if positive Y goes down, then this slope will be "backwards" from what you might expect from whether it's positive or negative
+    const m = nodeB.x === nodeA.x
+      ? null // infinite => vertical line
+      : (nodeB.y - nodeA.y) / (nodeB.x - nodeA.x)
+    // now solve for b...
+    // y = m*x + b
+    // y - m*x = b
+    const b = nodeA.y - m * nodeA.x
+    return {m, b}
+  }
+  const antinodesPresence = Array(matrixHeight).fill(null).map(() => Array(matrixWidth).fill(null).map(() => ' '))
+  Object.entries(antennas).forEach(([frequency, antennasAtFrequency]) => {
+    // console.log(frequency, antennasAtFrequency.map(a => `(${a.x},${a.y})`).join(' '))
+    // console.log(matrix.map(l => l.map(n => n === frequency ? frequency : ' ').join('')).join('\n'))
+    // console.log(Array(matrixWidth).fill('-').join(''))
+    antennasAtFrequency.forEach((antennaA, index) => {
+      if (!antennasAtFrequency[index + 1]) return // last antenna in the list
+      for (let cursor = index + 1; cursor < antennasAtFrequency.length; cursor++) { // O(n*log(n))
+        const antennaB = antennasAtFrequency[cursor]
+        const {m, b} = slope(antennaA, antennaB)
+        if (m === null)
+          for (let y = 0; y < matrixHeight; y++)
+            antinodesPresence[y][antennaA.x] = frequency
+        else
+          for (let x = 0; x < matrixWidth; x++) { // NOTE this should be optimized to only search within the map...
+            if (m === 0)
+              antinodesPresence[antennaA.y][x] = frequency
+            else {
+              let y = fudgeItToAccountForFloatingPointNonsense(m * x + b)
+              if (y >= 0 && y < matrixHeight) {
+                if (Math.floor(y) === y)
+                  antinodesPresence[y][x] = frequency
+
+              }
+            }
+          }
+      }
+    })
+    // console.log(antinodesPresence.map(l => l.join('')).join('\n'))
+  })
+  return antinodesPresence.reduce((totalSum, row) => {
+    return totalSum + row.reduce((lineSum, n) => n === ' ' ? lineSum : lineSum + 1, 0)
+  }, 0)
+}
+
 test('part 1', () => {
   expect(part1(sampleInput)).toEqual(14)
-  expect(part1(fullInput)).toEqual(14)
+  expect(part1(fullInput)).toEqual(364)
+})
+
+test('part 2', () => {
+  expect(fudgeItToAccountForFloatingPointNonsense(27.999999999999996)).toEqual(28)
+  expect(part2(`
+    ....x
+    .....
+    ..x..
+    .....
+    .....`)).toEqual(5)
+  expect(part2(`
+    ......
+    .x...x
+    ......`)).toEqual(6)
+  expect(part2(`
+    .v.
+    ...
+    ...
+    .v.`)).toEqual(4)
+  expect(part2(`
+    T.........
+    ...T......
+    .T........
+    ..........
+    ..........
+    ..........
+    ..........
+    ..........
+    ..........
+    ..........`)).toEqual(9)
+  expect(part2(`
+    T.........
+    ...T......
+    .T........
+    ..........
+    ..........
+    ..........
+    ......1...
+    ..........
+    ........10
+    ........0.`)).toEqual(20)
+  expect(part2(sampleInput)).toEqual(34)
+  expect(part2(fullInput)).toBe(1231)
 })
