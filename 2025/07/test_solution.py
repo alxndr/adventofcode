4804e15e831cdab7f0193376ac9a15595cac42a2
Alexander <alxndr+github@gmail.com>
2025-12-10 16:38:03 -0800

    2025 day 7: part 2 done 🎉 (python)

--- 2025/07/test_solution.py
+++ 2025/07/test_solution.py
@@ -5,11 +5,10 @@ BEAM_CHAR = '|'
 EMPTY_CHAR = '.'
 SPLITTER_CHAR = '^'
 
-class Conway:
+class Conway1:
     '''
     Sorta like a 1-D version of Conway's Game of Life…
     '''
-
     def __init__(self, width:int, start_location:int):
         if width <= 0:
             raise Error('need some width')
@@ -18,10 +17,8 @@ class Conway:
         self.field = [EMPTY_CHAR] * width
         self.field[start_location] = BEAM_CHAR
         self.count_of_splits = 0
-
     def __str__(self):
         return f"««{''.join(self.field)}»»"
-
     def split_at(self, position):
         if self.field[position] != BEAM_CHAR:
             return # no beam to split
@@ -31,7 +28,6 @@ class Conway:
         self.count_of_splits += 1
 
 def find_splitter_positions(line):
-    print('>', line)
     return [pos
             for pos, char
             in enumerate(line)
@@ -43,12 +39,48 @@ def part1(filename):
         for line in f.read().splitlines():
             if c:
                 print(c)
+                print('>', line)
                 splitter_positions = find_splitter_positions(line)
                 for splitter_pos in splitter_positions:
                     c.split_at(splitter_pos)
             else: # first run
-                c = Conway(width=len(line), start_location=line.find(START_CHAR))
+                c = Conway1(width=len(line), start_location=line.find(START_CHAR))
     return c.count_of_splits
+
+class Conway2:
+    '''
+    … no longer like the Game of Life tho
+    '''
+    def __init__(self, width:int, start_location:int):
+        self.field = [0] * width
+        self.field[start_location] = 1
+        self.count_of_timelines = 0
+    def __str__(self):
+        return f"««{'__'.join(str(i) for i in self.field)}»»"
+    def split_at(self, position):
+        if self.field[position] == 0:
+            return # no beam timelines to split
+        count_timelines = self.field[position]
+        self.field[position - 1] += count_timelines
+        self.field[position    ]  = 0
+        self.field[position + 1] += count_timelines
+    def total_timelines(self):
+        return sum(self.field)
+
+def part2(filename):
+    # how many paths through a graph...
+    c = None
+    with open(filename) as f:
+        for line in f.read().splitlines():
+            if c:
+                print(c)
+                print('>', '__'.join(line))
+                splitter_positions = find_splitter_positions(line)
+                for splitter_pos in splitter_positions:
+                    c.split_at(splitter_pos)
+            else: # first run
+                c = Conway2(width=len(line), start_location=line.find(START_CHAR))
+    return c.total_timelines()
 
 #######################
 
@@ -58,8 +90,8 @@ def test_part1_sample():
 def test_part1_full():
     assert part1('input.txt') == 1633
 
-# def test_part2_sample():
-#     assert part2('sample.txt') == 'fill in'
+def test_part2_sample():
+    assert part2('sample.txt') == 40
 
-# def test_part2_full():
-#     assert part2('input.txt') == 'unknown'
+def test_part2_full():
+    assert part2('input.txt') == 34339203133559
